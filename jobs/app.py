bb1a993b67fc1daaebb31de036d310f091e987c8
Grosvin Martinez Jr <grosvin.martinez.jr@ibm.com>
2022-01-12 13:13:13 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-    if connection == None
-        connection = g._conneection = sqlite3.connect(PATH)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=())
 
 @app.route('/')
 @app.route('/jobs')
